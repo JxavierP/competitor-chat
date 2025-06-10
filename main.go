fbeba5deddf1974e0dae4dc2f93abf39309612d1
Jxavier Paul <182994886+JxavierP@users.noreply.github.com>
2025-06-10 10:22:52 -0300

    feat: added htmx

--- main.go
+++ main.go
@@ -2,18 +2,31 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/JxavierP/competitor-chat/components"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
-func main()  {
+func main() {
 	router := gin.Default()
 	router.HTMLRender = &TemplRender{}
-	
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3001", "https://hoppscotch.io"},
+		AllowMethods:     []string{"GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
+	router.Static("/static", "./static")
+
 	router.GET("/", func(c *gin.Context) {
 		c.Render(http.StatusOK, &TemplRender{
-			 http.StatusOK,
+			http.StatusOK,
 			components.Index(),
 		})
 	})
