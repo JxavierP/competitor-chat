c12933981e2ccd5fc0f4c6956faa698930150483
Jxavier Paul <182994886+JxavierP@users.noreply.github.com>
2025-06-29 07:32:09 -0300

    style: fixed chatarea from expanding its width with code snippets

--- web/pages/index_templ.go
+++ web/pages/index_templ.go
@@ -51,7 +51,7 @@ func Index() templ.Component {
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
-			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 2, "<div class=\"flex flex-col w-full justify-center items-center bg-rose-pine-surface\">")
+			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 2, "<div class=\"flex flex-col w-full bg-rose-pine-surface\">")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
